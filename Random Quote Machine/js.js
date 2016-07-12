22478c6a58f527e915cda7d516985f787b3e6e21
Thorgal Aegirsson <usa.rs@live.com>
2016-07-12 00:23:09 -0400

    -Add AJAX and fallback

--- Random Quote Machine/js.js
+++ Random Quote Machine/js.js
@@ -6,32 +6,310 @@
         var box = document.getElementById('box');
         var quoteBox = document.getElementById('quoteBox');
         var twitter = document.getElementById('twitter');
-        var tweetBtn = document.getElementById('tweetBtn');
+        var tweetBtn = document.getElementsByClassName('tweetBtn')[0];
         var nextBtn = document.getElementById('nextBtn');
         var tweet;
+        var quote;
+
+        //pre-populate the page
+        getQuote();
+
 
         //event listeners
         nextBtn.addEventListener('click', function () {
-            tweet = displayQuote(quotes, quoteBox);
-            tweetBtn.href = 'https://twitter.com/intent/tweet?text='+encodeURIComponent(tweet.text.substr(0, 118-tweet.author.length)+'-'+tweet.author)+'&via=ESAREnterprises&related=freecodecamp';
+            document.getElementsByClassName('footnote')[0].style.display = 'block';
+            document.getElementsByClassName('fallback')[0].style.display = 'none';
+            console.log('btn pressed');
+            getQuote();
         });
 
-        //select a quote
-        function pickRndm(length) {
-            var num = Math.floor(Math.random() * length);
-            return num;
-        }
+        // AJAX call to TheySaidSo
 
-        //display a quote
-        function displayQuote(array, output) {
-            var quoteText = output.getElementsByTagName('p');
-            var quote = array[pickRndm(array.length)];
-            quoteText[0].innerText = quote.text;
-            quoteText[1].innerText = '- ' + quote.author;
-            return quote;
+        function getQuote() {
+            //helper function - random from local quote DB
+
+            function pickRndm(length) {
+                var num = Math.floor(Math.random() * length);
+                return num;
+            }
+
+            //helper function - display the quote
+            //display a quote
+            function displayQuote(quote, output) {
+                var quoteText = output.getElementsByTagName('p');
+                quoteText[0].style.opacity = 0;
+                quoteText[1].style.opacity = 0;
+                quoteText[0].innerText = quote.quote;
+                quoteText[1].innerText = '- ' + quote.author;
+                let i = 0;
+                var fade = setInterval(function () {
+                    quoteText[0].style.opacity = i;
+                    quoteText[1].style.opacity = i
+                    i += 0.1;
+                    if (i === 1) clearInterval(fade);
+                }, 200);
+
+            }
+            //AJAX call for quotes from TheySaidSo
+
+            var xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    console.log('success');
+                    quote = JSON.parse(xhr.responseText).contents.quotes[0];
+                    document.getElementsByClassName('footnote')[0].style.display = 'block';
+                    document.getElementsByClassName('fallback')[0].style.display = 'none';
+                    displayQuote(quote, quoteBox);
+                    tweetBtn.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote.quote.substr(0, 118 - quote.author.length) + '-' + quote.author) + '&via=ESAREnterprises&related=freecodecamp';
+                } else if (xhr.readyState === 4 && xhr.status !== 200) {
+                    console.log('failure');
+                    console.log(xhr.responseText); //debugging
+                    quote = quoteList[pickRndm(quoteList.length)];
+                    document.getElementsByClassName('footnote')[0].style.display = 'none';
+                    document.getElementsByClassName('fallback')[0].style.display = 'block';
+                    displayQuote(quote, quoteBox);
+                    tweetBtn.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote.quote.substr(0, 118 - quote.author.length) + '-' + quote.author) + '&via=ESAREnterprises&related=freecodecamp';
+                    if (xhr.responseText) {
+                        document.getElementsByClassName('fallback')[0].innerHTML = JSON.parse(xhr.responseText).error.message + ' <br>AJAX failed, fallback to local quotes';
+                    } else {
+                        document.getElementsByClassName('fallback')[0].innerHTML = 'AJAX failed, fallback to local quotes';
+                    }
+                } else {
+                    console.log('total failure');
+                    quote = quoteList[pickRndm(quoteList.length)];
+                    quoteBox.children[0].innerText = 'Waiting for response...';
+                    document.getElementsByClassName('footnote')[0].style.display = 'none';
+                    document.getElementsByClassName('fallback')[0].style.display = 'block';
+                    document.getElementsByClassName('fallback')[0].innerHTML = 'AJAX failed, fallback to local quotes';
+                }
+                // displayQuote(quote, quoteBox);
+                // tweetBtn.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote.quote.substr(0, 118 - quote.author.length) + '-' + quote.author) + '&via=ESAREnterprises&related=freecodecamp';
+            }
+
+            xhr.open('GET', 'http://quotes.restREMOVE/qod.json', true);
+            xhr.send();
+            quoteBox.children[0].innerText = 'Waiting for response...';
         }
     }
 
     document.addEventListener('DOMContentLoaded', init);
 })();
 
+
+
+// WIKIQUOTE
+
+
+// var WikiquoteApi = (function () {
+
+//     var wqa = {};
+
+//     var API_URL = "http://en.wikiquote.org/w/api.php";
+
+//     /**
+//      * Query based on "titles" parameter and return page id.
+//      * If multiple page ids are returned, choose the first one.
+//      * Query includes "redirects" option to automatically traverse redirects.
+//      * All words will be capitalized as this generally yields more consistent results.
+//      */
+//     wqa.queryTitles = function (titles, success, error) {
+//         $.ajax({
+//             url: API_URL,
+//             dataType: "jsonp",
+//             data: {
+//                 format: "json",
+//                 action: "query",
+//                 redirects: "",
+//                 titles: titles
+//             },
+
+//             success: function (result, status) {
+//                 var pages = result.query.pages;
+//                 var pageId = -1;
+//                 for (var p in pages) {
+//                     var page = pages[p];
+//                     // api can return invalid recrods, these are marked as "missing"
+//                     if (!("missing" in page)) {
+//                         pageId = page.pageid;
+//                         break;
+//                     }
+//                 }
+//                 if (pageId > 0) {
+//                     success(pageId);
+//                 } else {
+//                     error("No results");
+//                 }
+//             },
+
+//             error: function (xhr, result, status) {
+//                 error("Error processing your query");
+//             }
+//         });
+//     };
+
+//     /**
+//      * Get the sections for a given page.
+//      * This makes parsing for quotes more manageable.
+//      * Returns an array of all "1.x" sections as these usually contain the quotes.
+//      * If no 1.x sections exists, returns section 1. Returns the titles that were used
+//      * in case there is a redirect.
+//      */
+//     wqa.getSectionsForPage = function (pageId, success, error) {
+//         $.ajax({
+//             url: API_URL,
+//             dataType: "jsonp",
+//             data: {
+//                 format: "json",
+//                 action: "parse",
+//                 prop: "sections",
+//                 pageid: pageId
+//             },
+
+//             success: function (result, status) {
+//                 var sectionArray = [];
+//                 var sections = result.parse.sections;
+//                 for (var s in sections) {
+//                     var splitNum = sections[s].number.split('.');
+//                     if (splitNum.length > 1 && splitNum[0] === "1") {
+//                         sectionArray.push(sections[s].index);
+//                     }
+//                 }
+//                 // Use section 1 if there are no "1.x" sections
+//                 if (sectionArray.length === 0) {
+//                     sectionArray.push("1");
+//                 }
+//                 success({ titles: result.parse.title, sections: sectionArray });
+//             },
+//             error: function (xhr, result, status) {
+//                 error("Error getting sections");
+//             }
+//         });
+//     };
+
+//     /**
+//      * Get all quotes for a given section.  Most sections will be of the format:
+//      * <h3> title </h3>
+//      * <ul>
+//      *   <li> 
+//      *     Quote text
+//      *     <ul>
+//      *       <li> additional info on the quote </li>
+//      *     </ul>
+//      *   </li>
+//      * <ul>
+//      * <ul> next quote etc... </ul>
+//      *
+//      * The quote may or may not contain sections inside <b /> tags.
+//      *
+//      * For quotes with bold sections, only the bold part is returned for brevity
+//      * (usually the bold part is more well known).
+//      * Otherwise the entire text is returned.  Returns the titles that were used
+//      * in case there is a redirect.
+//      */
+//     wqa.getQuotesForSection = function (pageId, sectionIndex, success, error) {
+//         $.ajax({
+//             url: API_URL,
+//             dataType: "jsonp",
+//             data: {
+//                 format: "json",
+//                 action: "parse",
+//                 noimages: "",
+//                 pageid: pageId,
+//                 section: sectionIndex
+//             },
+
+//             success: function (result, status) {
+//                 var quotes = result.parse.text["*"];
+//                 var quoteArray = []
+
+//                 // Find top level <li> only
+//                 var $lis = $(quotes).find('li:not(li li)');
+//                 $lis.each(function () {
+//                     // Remove all children that aren't <b>
+//                     $(this).children().remove(':not(b)');
+//                     var $bolds = $(this).find('b');
+
+//                     // If the section has bold text, use it.  Otherwise pull the plain text.
+//                     if ($bolds.length > 0) {
+//                         quoteArray.push($bolds.html());
+//                     } else {
+//                         quoteArray.push($(this).html());
+//                     }
+//                 });
+//                 success({ titles: result.parse.title, quotes: quoteArray });
+//             },
+//             error: function (xhr, result, status) {
+//                 error("Error getting quotes");
+//             }
+//         });
+//     };
+
+//     /**
+//      * Search using opensearch api.  Returns an array of search results.
+//      */
+//     wqa.openSearch = function (titles, success, error) {
+//         $.ajax({
+//             url: API_URL,
+//             dataType: "jsonp",
+//             data: {
+//                 format: "json",
+//                 action: "opensearch",
+//                 namespace: 0,
+//                 suggest: "",
+//                 search: titles
+//             },
+
+//             success: function (result, status) {
+//                 success(result[1]);
+//             },
+//             error: function (xhr, result, status) {
+//                 error("Error with opensearch for " + titles);
+//             }
+//         });
+//     };
+
+//     /**
+//      * Get a random quote for the given title search.
+//      * This function searches for a page id for the given title, chooses a random
+//      * section from the list of sections for the page, and then chooses a random
+//      * quote from that section.  Returns the titles that were used in case there
+//      * is a redirect.
+//      */
+//     wqa.getRandomQuote = function (titles, success, error) {
+
+//         var errorFunction = function (msg) {
+//             error(msg);
+//         };
+
+//         var chooseQuote = function (quotes) {
+//             var randomNum = Math.floor(Math.random() * quotes.quotes.length);
+//             success({ titles: quotes.titles, quote: quotes.quotes[randomNum] });
+//         };
+
+//         var getQuotes = function (pageId, sections) {
+//             var randomNum = Math.floor(Math.random() * sections.sections.length);
+//             wqa.getQuotesForSection(pageId, sections.sections[randomNum], chooseQuote, errorFunction);
+//         };
+
+//         var getSections = function (pageId) {
+//             wqa.getSectionsForPage(pageId, function (sections) { getQuotes(pageId, sections); }, errorFunction);
+//         };
+
+//         wqa.queryTitles(titles, getSections, errorFunction);
+//     };
+
+//     /**
+//      * Capitalize the first letter of each word
+//      */
+//     wqa.capitalizeString = function (input) {
+//         var inputArray = input.split(' ');
+//         var output = [];
+//         for (s in inputArray) {
+//             output.push(inputArray[s].charAt(0).toUpperCase() + inputArray[s].slice(1));
+//         }
+//         return output.join(' ');
+//     };
+
+//     return wqa;
+// } ());
